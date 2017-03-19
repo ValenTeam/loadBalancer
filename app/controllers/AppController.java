9101c1b629fc11747506e2489250db481f67e50a
luisplazas <lf.plazas10@uniandes.edu.co>
2017-03-19 18:36:24 -0500

    Simple test

--- app/controllers/AppController.java
+++ app/controllers/AppController.java
@@ -5,24 +5,32 @@ package controllers;
  */
 
 import com.avaje.ebean.Model;
+import com.google.inject.Inject;
 import controllers.base.EPController;
 import models.Hospital;
 import org.jongo.Jongo;
+import play.api.libs.ws.WSResponse;
+import play.libs.ws.WSClient;
 import play.mvc.Result;
 import scala.concurrent.java8.FuturesConvertersImpl;
 import uk.co.panaxiom.playjongo.PlayJongo;
 import util.EPJson;
 
 import java.util.List;
+import java.util.concurrent.CompletionStage;
 
 public class AppController extends EPController {
+
+    @Inject
+    private WSClient ws;
 
     /**
      * Finds all the hospitals
      * @return OK 200 with a list that may be empty if there are no hospitals.
      */
     public Result test() {
-        return redirect("http://hospital-arquisoft.herokuapp.com/test");
+        ws.url("http://hospital-arquisoft.herokuapp.com/medicion").setContentType("application/json").post(request().body().asText());
+        return ok();
     }
 
 }
